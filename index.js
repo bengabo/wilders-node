0b77264c8980b5e780580d3e18710d81c490f0d6
Benjamin Gaboury <ben@bengabo.com>
2019-10-02 14:28:31 +0200

    index.js modified

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-/* let wilders = ['Ben', 'Wamwam', 'You', 'Jon'];
-let colors = [yellow, blue, green, red];
-let bgColors = [bgMagenta, bgCyan , bgRed, bgGreen];
+/*
+let wilders = ['Ben', 'Wamwam', 'You', 'Jon'];
+let colors = [chalk.yellow, chalk.blue, chalk.green, chalk.red];
+let bgColors = [bgMagenta, bgCyan, bgRed, bgGreen];
 
 for (let i = 0; i < wilders.length; i++){
   const chalk = require('chalk');
-  console.log(chalk.colors[i].bgColors[i](wilders[i]));
-} */
+  console.log(colors[i].bgColors[i](wilders[i]));
+};
+*/
+
 
 const chalk = require('chalk');
-
 console.log(chalk.yellow.bgBlue.bold('Bulma'));
 console.log(chalk.blue.bgMagenta.bold('Chirac'));
 console.log(chalk.green.bgRed.bold('Conan'));
